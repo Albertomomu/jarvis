09988b077deb86cc046f82f148747155895b2c89
Alberto <alberto.moreno2357@gmail.com>
2024-12-04 20:57:52 +0100

    feat: adding new favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import { twMerge } from "tailwind-merge";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "AI Startup Landing Page",
-  description: "A landing page for an AI startup created with Frontend Tribe",
+  title: "Jarvis Agency",
+  description: "Tu nueva agencia de marketing digital",
 };
 
 export default function RootLayout({
