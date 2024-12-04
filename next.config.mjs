e7a35e0d64fbe9fd9404bbe260c62be9e508ded0
Alberto <alberto.moreno2357@gmail.com>
2024-12-04 09:26:58 +0100

    feat: adding million dev & logoTicker animation

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+import MillionLint from "@million/lint";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
@@ -29,4 +30,7 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default MillionLint.next({
+  enabled: true,
+  rsc: true
+})(nextConfig);
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,9 +1,11 @@
+'use client';
 import acmeLogo from "@/assets/logo-acme.png";
 import apexLogo from "@/assets/logo-apex.png";
 import celestialLogo from "@/assets/logo-celestial.png";
 import quantumLogo from "@/assets/logo-quantum.png";
 import pulseLogo from "@/assets/logo-pulse.png";
 import echoLogo from "@/assets/logo-echo.png";
+import { motion } from "framer-motion";
 
 export const LogoTicker = () => {
   return (
@@ -13,13 +15,17 @@ export const LogoTicker = () => {
           <div className="flex-1 md:flex-none">
             <h2>Trabajan con Jarvis</h2>
           </div>
-          <div className="flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-            <div className="flex flex-none gap-14">
-              {[acmeLogo, apexLogo, celestialLogo, quantumLogo, pulseLogo, echoLogo].map((logo, index) => (
+          <div className="flex flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+            <motion.div
+              initial={{ translateX: "-50%" }}
+              animate={{ translateX: "0" }}
+              transition={{ duration: 30, ease: "linear", repeat: Infinity }}
+              className="flex flex-none gap-14 pr-14 -translate-x-1/2 ">
+              {[acmeLogo, apexLogo, celestialLogo, quantumLogo, pulseLogo, echoLogo, acmeLogo, apexLogo, celestialLogo, quantumLogo, pulseLogo, echoLogo,].map((logo, index) => (
                 // eslint-disable-next-line @next/next/no-img-element
                 <img key={index} src={logo.src} alt="Logo" className="h-6 w-auto" />
               ))}
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
